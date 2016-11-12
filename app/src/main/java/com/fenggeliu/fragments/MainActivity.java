f635057fcebba39bde7b16a8ed1e655f568c493a
Fengge <ryufengge@gmail.com>
2016-11-12 15:02:41 -0800

    Initial commit

---
+++ app/src/main/java/com/fenggeliu/fragments/MainActivity.java
@@ -0,0 +1,20 @@
+package com.fenggeliu.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements TopSectionFragment.TopSectionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // gets called when user clicks the button
+    @Override
+    public void createMeme(String top, String bottom) {
+        BottomPictureFragment bottomFragment = (BottomPictureFragment) getSupportFragmentManager().findFragmentById(R.id.fragment2);
+        bottomFragment.setMemeText(top, bottom);
+    }
+}
